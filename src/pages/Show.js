c7b5c59733ff7754c6713ca68c5684a567bf2a86
Jason Park <platosw83@gmail.com>
2021-11-01 21:16:48 -0400

    wip: update edit and delete (handleChange and handleSubmit)

--- src/pages/Show.js
+++ src/pages/Show.js
@@ -1,6 +1,30 @@
+import { useState } from 'react';
 
 
 function Show (props) {
+    const id = props.match.params.id;
+    const blogs = props.blogs;
+    const blog = blogs.find((b) => b._id === id);
+
+    // state for form
+    const [editForm, setEditForm] = useState(blog);
+
+    // handleChange function for form
+    const handleChange = (e) => {
+        setEditForm((prevState) => ({
+            ...prevState,
+            [e.target.name]: e.target.value,
+        }));
+    }
+
+    // handleSubmit for form
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        props.updateBlog(editForm, blog._id);
+        // redirect blogs back to index
+        props.history.push('/blog');
+    };
+
     return <h1>Show</h1>;
 }
 
