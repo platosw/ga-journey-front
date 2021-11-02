5307eda39c63af87faf4c17aa35cfae18c9e978c
Jason Park <platosw83@gmail.com>
2021-11-02 08:55:37 -0400

    Fix a bug

--- src/pages/Show.js
+++ src/pages/Show.js
@@ -33,6 +33,9 @@ function Show (props) {
     return (
         <div className="blog">
             <h1>{blog.title}</h1>
+            <button id='delete' onClick={removeBlog}>  {/* 왜 id='delete' 라고 쓰는지 물어보기 */}
+                DELETE
+            </button>
             <p>{blog.text}</p>
             <img src={blog.img} alt={blog.title} />
             <form onSubmit={handleSubmit}>
@@ -59,9 +62,6 @@ function Show (props) {
                 />
                 <input type="submit" value="Update Blog" />
             </form>
-            <button id='delete' onClick={removeBlog}>  {/* 왜 id='delete' 라고 쓰는지 물어보기 */}
-                DELETE
-            </button>
         </div>
     );
 }
